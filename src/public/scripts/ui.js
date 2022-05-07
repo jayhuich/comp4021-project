4ee28cf4e3446f1e32201c84a2edbc39cb13f006
javier <29498758+javier-hui@users.noreply.github.com>
2022-05-08 05:02:57 +0800

    wpm tracking, car movement, word detection

--- src/public/scripts/ui.js
+++ src/public/scripts/ui.js
@@ -20,7 +20,7 @@ const SignInForm = (() => {
         // submit event for the signin form
         $("#signin-form").on("submit", (e) => {
 
-            // do not submit the form
+            // prevent submitting the form
             e.preventDefault();
 
             // get the input fields
@@ -41,23 +41,24 @@ const SignInForm = (() => {
 
         // submit event for the register form
         $("#register-form").on("submit", (e) => {
-            // Do not submit the form
+
+            // prevent submitting the form
             e.preventDefault();
 
-            // Get the input fields
+            // get the input fields
             const username = $("#register-username").val().trim();
             const displayName = $("#register-displayname").val().trim();
             const carId = 1;
             const password = $("#register-password").val().trim();
             const confirmPassword = $("#register-confirm").val().trim();
 
-            // Password and confirmation does not match
+            // check passwords
             if (password != confirmPassword) {
-                $("#register-message").text("Passwords do not match.");
+                $("#register-message").text("passwords don't match.");
                 return;
             }
 
-            // Send a register request
+            // send a register request
             Registration.register(username, displayName, carId, password,
                 () => {
                     $("#register-form").get(0).reset();
@@ -103,7 +104,7 @@ const UserPanel = (() => {
     const hide = () => $("#user-panel").hide();
 
     // updates user panel
-    const update = function (user) {
+    const update = (user) => {
         if (user) {
             $("#user-panel .user-displayname").text(`hello, ${user.displayName}!`);
         }
@@ -115,6 +116,91 @@ const UserPanel = (() => {
     return { initialize, show, hide, update };
 })();
 
+const GamePanel = (() => {
+    let gameParagraph = null;
+    let gameInput = null;
+
+    const initialize = () => {
+
+        // set up jquery objects
+        gameParagraph = $("#game-paragraph");
+        gameInput = $("#game-input");
+
+        // click event for ready
+        $("#game-ready-button").on("click", () => {
+            startGame([Authentication.getUser()], "Beginning today, treat everyone you meet as if they were going to be dead by midnight. Extend to them all the care, kindness and understanding you can muster, and do it with no thought of any reward. Your life will never be the same again.");
+            // Socket.ready();
+        });
+    };
+
+    // starts the game
+    const startGame = (players, paragraph) => {
+        const startTime = new Date();
+        const wordArray = paragraph.split(' ');
+        let currentWordIndex = 0;
+
+        const charCountArray = [];
+        let charCount = 0;
+        for (word of wordArray) {
+            charCount += word.length;
+            charCountArray.push(charCount);
+        }
+
+        let wpm = 0;
+
+        // clear racetracks
+        for (let i = 0; i < 4; i++) {
+            $(`#game-car-${i}`).hide();
+        }
+        gameParagraph.empty();
+
+        // get the current user
+        const currentUser = Authentication.getUser();
+
+        // add cars one by one
+        for (let i = 0; i < players.length; i++) {
+            $(`#game-car-${i}`).css("background-image", `url("img/car${players[i].carId}.png")`)
+            $(`#game-car-${i}`).show();
+        }
+        wordArray.forEach((word) => {
+            const wordSpan = $("<span></span>").text(word + ' ');
+            gameParagraph.append(wordSpan);
+        });
+
+        // handle keydown event
+        gameInput.keydown((e) => {
+            const inputValue = gameInput.val().trim();
+
+            if (e.key == ' ') {
+                if (inputValue == wordArray[currentWordIndex]) {
+                    wpm = Math.floor((charCountArray[currentWordIndex] / 5) / ((new Date() - startTime) / 60000));
+                    $(`#game-flexbox-${0}`).css("width", Math.floor(charCountArray[currentWordIndex]/paragraph.length * 100) + '%');
+                    $('#game-paragraph > span').eq(currentWordIndex).css("color", "grey");
+                    gameInput.val('');
+                    currentWordIndex++;
+                }
+                else {
+                    $('#game-paragraph > span').eq(currentWordIndex).css("color", "red");
+                }
+                if (currentWordIndex >= wordArray.length) {
+                    console.log('end, your wpm is ' + wpm);
+                }
+            }
+
+            // call function in socket.js to emit "typing" event
+            // Socket.typingMessage();
+        });
+    };
+
+    // typing
+    const typing = () => {
+
+
+    };
+
+    return { initialize, startGame };
+})();
+
 
 const UI = (() => {
     const getUserDisplay = (user) => {
@@ -123,7 +209,7 @@ const UI = (() => {
     };
 
     // all components of ui
-    const components = [SignInForm, UserPanel];
+    const components = [SignInForm, UserPanel, GamePanel];
 
     const initialize = () => {
         for (const component of components) {
